a114b255cf58258953d2827976d36398ca269106
owen-yator <yatorowen@gmail.com>
2025-02-07 15:35:28 +0300

    Updated login Activity.

--- app/src/main/java/com/example/loginsignupfrd/LoginActivity.kt
+++ app/src/main/java/com/example/loginsignupfrd/LoginActivity.kt
@@ -1,20 +1,77 @@
 package com.example.loginsignupfrd
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.loginsignupfrd.databinding.ActivityLoginBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+
+        binding.loginButton.setOnClickListener {
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+
+            if (loginUsername.isNotEmpty() && loginPassword.isNotEmpty()){
+                loginuser(loginUsername,loginPassword)
+            }else{
+                Toast.makeText(this@LoginActivity,"All Fields Are Mandatory",Toast.LENGTH_SHORT).show()
+
+            }
+
         }
+        binding.signupRedirect.setOnClickListener{
+            startActivity(Intent(this@LoginActivity,SignupActivity::class.java))
+            finish()
+        }
+
+    }
+    private fun loginuser(username:String,password:String){
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                if (datasnapshot.exists()){
+                    for(userSnapshot in datasnapshot.children){
+                        val userData = userSnapshot.getValue(UserData::class.java)
+
+                        if (userData != null && userData.password == password){
+                            Toast.makeText(this@LoginActivity,"Login Successful",Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this@LoginActivity,MainActivity::class.java))
+                            finish()
+                            return
+                        }
+                    }
+
+                }
+                Toast.makeText(this@LoginActivity,"Login Failed",Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@LoginActivity,"Database Error:${databaseError.message}",Toast.LENGTH_SHORT).show()
+
+
+            }
+        })
     }
 }
